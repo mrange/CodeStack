33a40a0e024e9b5f1c4446e2a4409ca8f8d9a13e
mrange <marten_range@hotmail.com>
2013-09-14 11:07:51 +0200

    1. Added initial response for q18787460

---
+++ q18787460/ModelGenerator/ModelGenerator/Generated.cs
@@ -0,0 +1,61 @@
+﻿// ############################################################################
+// #                                                                          #
+// #        ---==>  T H I S  F I L E  I S   G E N E R A T E D  <==---         #
+// #                                                                          #
+// # This means that any edits to the .cs file will be lost when its          #
+// # regenerated. Changes should instead be applied to the corresponding      #
+// # template file (.tt)                                                      #
+// ############################################################################
+
+
+
+
+
+
+
+
+
+
+
+namespace ModelGenerator
+{
+    /// <summary>
+    /// Poco class for CUS_Customer
+    /// </summary>
+    partial class CUS_Customer
+    {
+        public System.Int64                             CUS_ID                                  
+        {
+            get;
+            set;
+        }
+
+        public System.String                            CUS_FirstName                           
+        {
+            get;
+            set;
+        }
+
+        public System.String                            CUS_LastName                            
+        {
+            get;
+            set;
+        }
+
+        public System.DateTime                          CUS_Born                                
+        {
+            get;
+            set;
+        }
+
+        public System.DateTime                          CUS_Created                             
+        {
+            get;
+            set;
+        }
+
+ 
+
+    }
+}
+
