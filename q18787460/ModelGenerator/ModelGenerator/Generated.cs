22b26ade68a425261d8f8e603bd96142955cdccf
mrange <marten_range@hotmail.com>
2013-09-16 21:30:43 +0200

    1. Updates to template a

--- q18787460/ModelGenerator/ModelGenerator/Generated.cs
+++ q18787460/ModelGenerator/ModelGenerator/Generated.cs
@@ -39,16 +39,11 @@ namespace ModelGenerator
     partial class CUS_Customer
     {
         public System.Int64                             CUS_ID                                  { get; set; }
-
         public System.String                            CUS_FirstName                           { get; set; }
-
         public System.String                            CUS_LastName                            { get; set; }
-
         public System.DateTime                          CUS_Born                                { get; set; }
-
         public System.DateTime                          CUS_Created                             { get; set; }
-
- 
+    } 
 
     /// <summary>
     /// Command class for CUS_Customer
@@ -56,19 +51,17 @@ namespace ModelGenerator
     partial class CUS_CustomerCommand : CommandBase, ICommand
     {
         public System.Int64                             CUS_ID                                   { get; set; }
-
         public System.String                            CUS_FirstName                            { get; set; }
-
         public System.String                            CUS_LastName                             { get; set; }
-
         public System.DateTime                          CUS_Born                                 { get; set; }
-
         public System.DateTime                          CUS_Created                              { get; set; }
-
  
     }
 
-    public class CUS_CustomerCommandHandler : ICommandHandler<CUS_CustomerCommand>
+    /// <summary>
+    /// Command handler class for CUS_Customer
+    /// </summary>
+    partial class CUS_CustomerCommandHandler : ICommandHandler<CUS_CustomerCommand>
     {
         private readonly IUsersRepository _repository;
         private readonly IUnitOfWork _unitOfWork;
@@ -82,20 +75,20 @@ namespace ModelGenerator
         {
             CUS_Customer entity;
 
-            if (command.CSO.CUS_ID == 0)
+            if (command.CUS_ID == 0)
             {
                 entity = AutoMapper.Mapper.Map<CUS_Customer>(command);
                 _repository.Add(entity);
             }
             else
             {
-                entity = _repository.Get(x=>x.UserId==command.CSO.CUS_ID);
+                entity = _repository.Get(x=>x.UserId==command.CUS_ID);
                 entity = AutoMapper.Mapper.Map<CUS_Customer>(command);
                 _repository.Update(entity);
             }
-            _unitOfWork.Commit(command.CSO.CUS_ID);         
+            _unitOfWork.Commit(command.CUS_ID);         
 
-            return new CommandResult(true,entity.CSO.CUS_ID);
+            return new CommandResult(true,entity.CUS_ID);
         }
     }
 }
